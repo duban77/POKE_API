6aaee649b71eb5327dfeee93cc31c40ddbc864d0
duban77 <159207588+duban77@users.noreply.github.com>
2025-04-23 22:55:00 +0000

    222

--- js/api.js
+++ js/api.js
@@ -1,15 +1,11 @@
-function mostrarApi(){
-    const app = document.getElementById("app");
-    app.innerHTML = "Api";
-}
+let pokemones = [];
+let totalPokes = 1026; /* debe agregarse uno mas ya q el indice comienza en 0 */
 
 async function  conexionLista() {
     const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1025')
     const data = await res.json()
-    return data.results;
+    pokemones = data.results;
+    mostrarLista(pokemones)
 }
 
-async function General() {
-    const infoPokes = await conexionLista();
-    mostrarHome(infoPokes) 
-}
+conexionLista()
